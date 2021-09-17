69febc79f95f8c1fda0bb447490765afda32b292
Marcos Javier Riganti <marcos.riganti@gmail.com>
2021-09-17 18:37:31 +0200

    adding facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,32 @@
 import { useEffect } from "react";
 import TagManager from "react-gtm-module";
+import ReactPixel from "react-facebook-pixel";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 const tagManagerArgs = {
   gtmId: "GTM-GTM-5V6FR3",
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   useEffect(() => {
     TagManager.initialize(tagManagerArgs);
   }, []);
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then(x => x.default)
+      .then(ReactPixel => {
+        ReactPixel.init("545425799568971"); // facebookPixelId
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
 
   return <Component {...pageProps} />;
 }
